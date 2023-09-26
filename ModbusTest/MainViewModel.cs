b29dcce24c64b233993be7ff1ef3bb4ca5db1c99
Patrik Šoma <soma@hunterdouglas.cz>
2023-09-26 08:51:58 +0200

    Add project files.

---
+++ ModbusTest/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using EasyModbus;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ModbusTest
+{
+    public class MainViewModel : PrimeViewModel
+    {
+        private string _Weight;
+        public string Weight { get => _Weight; set => SetProperty(ref _Weight,value); }
+
+        ModbusClient client;
+
+        public MainViewModel()
+        {
+            client = new ModbusClient("127.0.0.1", 502); // 192.168.0.254
+            AsyncInit();
+        }
+
+        private async void AsyncInit()
+        {
+            ReadWeight();
+        }
+
+        private async Task ReadWeight()
+        {
+            while (true)
+            {
+                try
+                {
+                    decimal w = -1;
+
+                    if (!client.Connected)
+                        client.Connect();
+                    else
+                        w = ((decimal)client.ReadHoldingRegisters(10, 1)[0]) / 100;
+
+                    Weight = w.ToString();
+
+                }
+                catch {}
+                finally 
+                {
+                    await Task.Delay(3000);
+                }
+
+            }
+        }
+    }
+}
---
+++ ModbusTest/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using EasyModbus;
+
+namespace ModbusTest
+{
+    public partial class MainWindow : Window
+    {
+       MainViewModel viewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = viewModel = new MainViewModel();
+           
+        }
+
+
+    }
+}
